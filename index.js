35f8c3fae22a9f1e1dfddece3162734921f82742
VikashAlex <vkumar637525@gmail.com>
2025-04-26 21:05:17 +0530

    bug fix

--- index.js
+++ index.js
@@ -13,77 +13,91 @@ let previousValue = "";
 
 const ShowData = (show) => {
     console.log(show);
-    if (show.cod === 200) {
 
-        card.classList.add("inc-height")
-        weatherBox.style.display = "block"
-       setTimeout(() => {
+    card.classList.add("inc-height")
+    weatherBox.style.display = "block"
+    setTimeout(() => {
         weatherBox.classList.add("weather-show")
-       }, 100);
-        errorBox.style.display = "none"
-        let imgPath = show.weather[0].icon;
-        let temp = Math.floor(show.main.temp);
-        let humidity = show.main.humidity;
-        let wind = Math.floor(show.wind.speed);
+    }, 100);
+    errorBox.style.display = "none"
+    let imgPath = show.weather[0].icon;
+    let temp = Math.floor(show.main.temp);
+    let humidity = show.main.humidity;
+    let wind = Math.floor(show.wind.speed);
 
-        // Update All Details 
-        document.querySelector(".weather img").src = `https://openweathermap.org/img/wn/${imgPath}@2x.png`;
-        document.querySelector(".temp").innerHTML = `${temp}°C`;
-        document.querySelector(".city").innerHTML = show.name;
-        document.querySelector(".humidity").innerHTML = `${humidity}%`;
-        document.querySelector(".wind").innerHTML = `${wind} km/h`;
-        
-    }
-    
-    else {
-        errorBox.style.display = "block"
-        weatherBox.style.display = "none"
-        card.classList.remove("inc-height")
-        error.innerHTML = `Invalid city name ${previousValue}`
-    }
+    // Update All Details 
+    document.querySelector(".weather img").src = `https://openweathermap.org/img/wn/${imgPath}@2x.png`;
+    document.querySelector(".temp").innerHTML = `${temp}°C`;
+    document.querySelector(".city").innerHTML = show.name;
+    document.querySelector(".humidity").innerHTML = `${humidity}%`;
+    document.querySelector(".wind").innerHTML = `${wind} km/h`;
+
 
 }
 
 const getInput = async () => {
-    const cityName = inputBox.value;
-    const Response = await fetch(`${apiUrl}${cityName}&appid=${apiKey}`)
-    const data = await Response.json()
-    ShowData(data)
+    if (inputBox.value != previousValue) {
+        const cityName = inputBox.value;
+        const Response = await fetch(`${apiUrl}${cityName}&appid=${apiKey}`)
+        const data = await Response.json()
+        if (data.cod === 200) {
+            ShowData(data)
+            console.log("yes");
+            previousValue = inputBox.value;
+            inputBox.value = ""
+
+        }
+        else {
+            errorBox.style.display = "block"
+            setTimeout(() => {
+                weatherBox.classList.remove("weather-show")
+            }, 100);
+            weatherBox.style.display = "none"
+            card.classList.remove("inc-height")
+            error.innerHTML = `Invalid city name ${inputBox.value}`
+            inputBox.value = ""
+            previousValue=""
+        }
+    }
+    else {
+        inputBox.value = ""
+    }
+
+
 
 
 }
 
+
+
 inputBox.addEventListener("keyup",
     function (e) {
         if (e.code == "Enter") {
-            if (inputBox.value !== previousValue) {
-                getInput()
-                previousValue = inputBox.value;
-                inputBox.value = ""
-            }
-            else {
+            if (inputBox.value === "") {
                 error.innerHTML = "Please Enter The City Name";
                 errorBox.style.display = "block"
                 weatherBox.style.display = "none"
                 card.classList.remove("inc-height")
-                
+            }
+            else {
+                getInput()
+               
             }
         }
     }
 )
+
+
 searchBtn.addEventListener("click", function () {
-    if (inputBox.value !== previousValue) {
-        getInput()
-        previousValue = inputBox.value;
-        inputBox.value = ""
-    }
-    else {
-        error.innerHTML = "Please Enter The City Name";
-        errorBox.style.display = "block"
-        weatherBox.style.display = "none"
-        card.classList.remove("inc-height")
-        
-    }
-  
+   if (inputBox.value === "") {
+    error.innerHTML = "Please Enter The City Name";
+    errorBox.style.display = "block"
+    weatherBox.style.display = "none"
+    card.classList.remove("inc-height")
+}
+else {
+    getInput()
+}
+
 })
 
